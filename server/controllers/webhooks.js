711d0896f5f038084a42af9c7f14a599d00cfbbb
Soumyaa30 <soumyawalia30@gmail.com>
2025-05-12 01:19:26 +0530

    file name corrected

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,5 +1,5 @@
 import { Webhook } from "svix";
-import User from "../models/user.js"
+import User from "../models/User.js"
 import Stripe from "stripe";
 import { Purchase } from "../models/Purchase.js";
 import Course from "../models/Course.js";
