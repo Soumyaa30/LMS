c98edf8e7c1a8a83b7caacca7df2db7449f850c0
Soumyaa30 <soumyawalia30@gmail.com>
2025-05-12 02:02:48 +0530

    request-response corrected

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -67,15 +67,15 @@ export const clerkWebHooks = async (req, res)=>{
 const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export const stripeWebhooks = async(req, res) =>{
-    const sig = request.headers['stripe-signature'];
+    const sig = req.headers['stripe-signature'];
 
   let event;
 
   try {
-    event = Stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+    event = Stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
   }
   catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
+    res.status(400).send(`Webhook Error: ${err.message}`);
   }
 
   // Handle the event
@@ -125,5 +125,5 @@ export const stripeWebhooks = async(req, res) =>{
   }
 
   // Return a response to acknowledge receipt of the event
-  response.json({received: true});
+  res.json({received: true});
 }
